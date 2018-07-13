7af5d7118b67678de0433f64200e4b67565a40c7
tuhnik <huntlarsen@gmail.com>
2018-07-13 14:06:10 +0300

    ...

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var image = document.createElement("IMG");
 image.src="aardvark.svg";
 image.setAttribute('class', 'aardvark');
-$("body").append(image);
+
 
 
 $( document ).ready(()=>{
     particlesJS.load('particles-js', 'assets/particles.json', function() {
-        console.log('callback - particles.js config loaded');
+        $("body").append(image);
       });
 
 })
