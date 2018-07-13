17f72bb3f7030cf470b17122081218617707c74f
tuhnik <huntlarsen@gmail.com>
2018-07-13 14:02:52 +0300

    ...

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// var image = document.createElement("IMG");
-// image.src="aardvark.svg";
-// image.setAttribute('class', 'aardvark');
-// $("body").html(image);
+var image = document.createElement("IMG");
+image.src="aardvark.svg";
+image.setAttribute('class', 'aardvark');
+$("body").html(image);
 
 
-// $( document ).ready(()=>{
-//     particlesJS.load('particles-js', 'assets/particles.json', function() {
-//         console.log('callback - particles.js config loaded');
-//       });
+$( document ).ready(()=>{
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
 
-// })
+})
 
 
 
