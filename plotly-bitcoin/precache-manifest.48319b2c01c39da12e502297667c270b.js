7839ab9a4f3160d17bc2002e748eac9cc751b67c
tuhnik <huntlarsen@gmail.com>
2018-10-19 06:31:03 +0300

    new project

---
+++ plotly-bitcoin/precache-manifest.48319b2c01c39da12e502297667c270b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5f2a9601a26374cdf3a2",
+    "url": "./static/js/main.5f2a9601.chunk.js"
+  },
+  {
+    "revision": "af36a770b37cc5d39e4e",
+    "url": "./static/js/1.af36a770.chunk.js"
+  },
+  {
+    "revision": "5f2a9601a26374cdf3a2",
+    "url": "./static/css/main.2a76bf47.chunk.css"
+  },
+  {
+    "revision": "06feccf44170bd402367b23a3c949a38",
+    "url": "./index.html"
+  }
+];
